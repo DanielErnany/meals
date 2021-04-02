c731402ca133acb4ac86bbe080f72836055b6dc9
Daniel Ernany <ddernany@gmail.com>
2021-04-02 13:31:40 -0300

    TabBar na parte inferior

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,40 +2,53 @@ import 'package:flutter/material.dart';
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
 
-class TabsScreem extends StatelessWidget {
+class TabsScreem extends StatefulWidget {
+  @override
+  _TabsScreemState createState() => _TabsScreemState();
+}
+
+class _TabsScreemState extends State<TabsScreem> {
+  int _selectedIndexScreen = 0;
+  final List<Widget> _screens = [
+    CategoriesScreen(),
+    FavoriteScreen(),
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedIndexScreen = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // Widget para criar tabbar
-    return DefaultTabController(
-      // Numero de abas que o tabbar vai ter
-      length: 2,
-      // Define a aba inicial
-      initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Vamos Cozinhar'),
-          centerTitle: true,
-          // Cria o tabbar
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favoritos',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar'),
+        centerTitle: true,
+      ),
+      body: _screens[_selectedIndexScreen],
+      // Cria uma tabbar na parte inferior
+      bottomNavigationBar: BottomNavigationBar(
+        // cor quando o item ñ está selecionado
+        unselectedItemColor: Colors.white,
+        // cor quando o item está selecionado
+        selectedItemColor: Theme.of(context).accentColor,
+        // Mostra qual dos itens está selecionado
+        currentIndex: _selectedIndexScreen,
+        onTap: _selectScreen,
+        backgroundColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text('Categorias'),
           ),
-        ),
-        // Widget para ver as abas do tabbar
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavoriteScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            title: Text('Favoritos'),
+          ),
+        ],
       ),
     );
   }
